32068d5ab2a63a12d86972560c5c68cb0d1e2139
Leonardo Almeida <leonardoaesilva@gmail.com>
2023-03-10 21:49:05 -0300

    remoção de importação

--- src/main/java/br/com/ada/projetoweblocadora/controller/ClienteController.java
+++ src/main/java/br/com/ada/projetoweblocadora/controller/ClienteController.java
@@ -4,7 +4,6 @@ import br.com.ada.projetoweblocadora.model.Cliente;
 import br.com.ada.projetoweblocadora.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
